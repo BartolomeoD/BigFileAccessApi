7684dd48feb4b397ebe735f6387c1c8294302dd3
Гатауллин Диас <dias.gataullin@akbarsdigital.ru>
2018-11-27 17:24:07 +0300

    init

---
+++ BigFileAccessApi/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigFileAccessApi.Controllers
+{
+    [ApiController]
+    public class FileController : Controller
+    {
+        [HttpGet("")]
+        public string index()
+        {
+            return "it`s ok";
+        }
+    }
+}
